e622a5866184de564fdd96e8580e84d2aaa0361d
tindy360 <tindy360>
2017-06-20 11:29:40 -0400

    working cord data GET

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {
   Route
 } from 'react-router-dom';
 import Splash from '../src/Components/Splash/Splash';
-import Breweries from '../src/Components/Breweries/Breweries';
+import Brewery from '../src/Components/Brewery/Brewery';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +13,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path={'/'} component={Splash} />
-          <Route exact path={'/breweries/:brewery'} component={Breweries} />
+          <Route exact path={'/brewery/:brewery'} component={Brewery} />
         </div>
       </Router>
     );
